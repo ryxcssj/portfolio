72efbd640d7d881bebc23e0fdfe5109f2429e58d
ryxcssj <matiasifnacioavaloscordero18@gmail.com>
2025-06-16 12:08:01 -0400

    Subida inicial del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const hamburger=document.getElementById('hamburger');
+const navMenu=document.getElementById('nav-menu');
+if(hamburger){hamburger.addEventListener('click',()=>navMenu.classList.toggle('show'));}
+const toggleBtn=document.getElementById('dark-toggle');
+if(toggleBtn){toggleBtn.addEventListener('click',()=>document.documentElement.classList.toggle('dark-mode'));}
